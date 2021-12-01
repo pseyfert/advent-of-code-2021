cc3b8f2fa1c5a810a803cd835017523b9dab892e
Paul Seyfert <pseyfert.mathphys@gmail.com>
2021-12-01 14:49:13 +0100

    part 2

---
+++ 01/sliding_window_sweep.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+zmodload zsh/mapfile
+local -a measurements
+measurements=("${(f)mapfile[$1]}")
+
+# For the sliding window most of the values in the window don't actually matter.
+# In the series a,b,c,d the sums a+b+c and b+c+d only differ in a vs d.
+local -i increased
+increased=0
+for (( b=4 ; b<$#measurements; b++ )); do
+  [[ $measurements[$b] -gt $measurements[$b-3] ]] && (( increased++ ))
+  prev=$m
+done
+print $increased
